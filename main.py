a40f0714d112db66a498e4d66fbb4c83e70b4d8d
MaximOksiuta <63787095+MaximOksiuta@users.noreply.github.com>
2024-08-23 16:50:03 +0300

    extract from pdf editor

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import logging
-from aiogram import Bot, Dispatcher, types
+import os
+
+from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
-from aiogram.types import Message
-from commands.info import process_help_command, process_start_command
+from dotenv import load_dotenv
+
+from commands.callback_handler import process_callback
+from commands.handlers import router
 from commands.handlers_dice import send_d4_image, send_d6_image, send_d8_image, send_d10_image, send_d12_image, \
     send_d20_image, send_d100_image
-from commands.pdf_editor import handle_docs, process_callback
+from commands.info import process_help_command, process_start_command
+from commands.pdf_editor import handle_docs
 from commands.text_commands import process_text_input  # комент если нужен гпт
-from commands.handlers import router
-
-import os
-from dotenv import load_dotenv
 
 load_dotenv()
 
